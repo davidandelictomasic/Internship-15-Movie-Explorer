444996e185f62f39c9a61a1ef1a38d4291c67da6
davidandelictomasic <david.andelic.tomasic@gmail.com>
2026-03-01 13:11:58 +0100

    Add search movies feature

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,20 +1,47 @@
+import { useState, useRef, useEffect } from 'react'
 import useFetchMovies from '../hooks/useFetchMovies'
 import MovieCard from '../components/MovieCard'
 import '../styles/Movies.css'
 
 function Movies() {
   const { movies, loading, error } = useFetchMovies()
+  const [filter,setFilter] = useState('')
+  const [search, setSearch] = useState('')
+  const searchRef = useRef(null)
 
+  useEffect(() => {
+    searchRef.current.focus()
+  }, [])
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(search.toLowerCase())
+  )
+  
   return (
     <div>
       <h1>Movies</h1>
+      <input
+        ref={searchRef}
+        type="text"
+        placeholder="Search movies..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <label htmlFor="select">Sort by</label>
+      <select name='select' value={filter} onChange={(e)=>setFilter(e.target.value)}>
+        <option value=""></option>
+        <option value="title">Title</option>
+        <option value="release_date">Release Date</option>
+        <option value="rating">Rating</option>
+      </select>
+      <p>Current filter {filter}</p>
       {loading && <p>Loading movies...</p>}
       {error && <p>{error}</p>}
-      {!loading && !error && movies.length === 0 && (
+      {!loading && !error && filteredMovies.length === 0 && (
         <p>No movies found.</p>
       )}
       <div className="grid">
-        {movies.map((movie) => (
+        {filteredMovies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
